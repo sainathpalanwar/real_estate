6b1efd75fa7284dab9d0ea0d69dcdea74c289d12
sainathpalanwar <35864974+sainathpalanwar@users.noreply.github.com>
2020-06-03 23:55:34 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,156 @@
+<footer class="footer">
+    <div class="container footer-inner">
+        <div class="row">
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                <div class="footer-item">
+                    <h4>Contact Us</h4>
+
+                    <ul class="contact-info">
+                        <li> Project Nandi, Hingoli Gate,
+                          Beside Manganani Function Hall,
+                           Venkatesh Nagar,
+                            Nanded
+                        </li>
+                        <li>
+                            Email: <a href="palanwars@gmail.com">ashish.badalwad@gmail.com</a>
+                        </li>
+                        <li>
+                            Phone: <a href="tel:+917720865111">+917720865111</a>
+                        </li>
+						 <li>
+                            Phone: <a href="tel:+917720865333">+917720865333</a>
+                        </li>
+                    </ul>
+
+                    <ul class="social-list clearfix">
+                        <li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#" class="google"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#" class="rss"><i class="fa fa-rss"></i></a></li>
+                        <li><a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xl-2 col-lg-2 col-md-6 col-sm-6">
+                <div class="footer-item">
+                    <h4>
+                        Useful Links
+                    </h4>
+                    <ul class="links">
+                        <li>
+                            <a href="about.php"><i class="fa fa-angle-right"></i>About us</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-angle-right"></i>Service</a>
+                        </li>
+                        <li>
+                            <a href="properties.php"><i class="fa fa-angle-right"></i>Properties</a>
+                        </li>
+                        <li>
+                            <a href="contact.php"><i class="fa fa-angle-right"></i>Contact Us</a>
+                        </li>
+                        <!--
+                        <li>
+                            <a href="#"><i class="fa fa-angle-right"></i>Property Details</a>
+                        </li>-->
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6">
+                <div class="recent-posts footer-item">
+                    <h4>Recent Properties</h4>
+                    <?php
+				   $query1=mysqli_query($con,"select * from property ORDER BY RAND() LIMIT 2");
+
+				   while( $res1=mysqli_fetch_array($query1))
+				   {
+					   
+				   $property_id=$res1['id'];
+				   $img=$res1['image'];
+				   
+				  ?> 
+				  <div class="media mb-4"> <a href="properties-details.html"> <img src="admin/images/property_image/<?php echo $img ; ?>" alt="sub-property">
+                            </a>
+                            <div class="media-body align-self-center">
+                                <h5>
+                                    <a href="properties-details.html"> <?php echo $res1['title']; ?></a>
+                                </h5>
+                                <p> <?php echo $res1['address'];?></p>
+                                
+                            </div>
+                        </div><?php
+				   }
+				   
+				   
+				   ?>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6">
+                <div class="footer-item clearfix">
+                    <h4>Subscribe</h4>
+                    <div class="Subscribe-box">
+                        <p></p>
+                        <form action="#" method="GET">
+                            <p>
+                                <input type="text" class="form-contact" name="email" placeholder="Enter Address">
+                            </p>
+                            <p>
+                                <button type="submit" name="submitNewsletter" class="btn btn-block btn-color">
+                                    Subscribe
+                                </button>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xl-12">
+                <p class="copy">&copy;  All Rights Reserved For Project Nandi 2020 <a href="index.php" target="_blank"></a>   Developer : Sainath Palanwar</p>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer end -->
+
+<!-- Full Page Search -->
+<div id="full-page-search">
+    <button type="button" class="close">×</button>
+    <form action="#">
+        <input type="search" value="" placeholder="type keyword(s) here" />
+        <button type="button" class="btn btn-sm btn-color">Search</button>
+    </form>
+</div>
+
+
+
+<!-- External JS libraries -->
+<script src="assets/js/jquery-2.2.0.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/jquery.selectBox.js"></script>
+<script src="assets/js/rangeslider.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/jquery.filterizr.js"></script>
+<script src="assets/js/wow.min.js"></script>
+<script src="assets/js/backstretch.js"></script>
+<script src="assets/js/jquery.countdown.js"></script>
+<script src="assets/js/jquery.scrollUp.js"></script>
+<script src="assets/js/particles.min.js"></script>
+<script src="assets/js/typed.min.js"></script>
+<script src="assets/js/dropzone.js"></script>
+<script src="assets/js/jquery.mb.YTPlayer.js"></script>
+<script src="assets/js/leaflet.js"></script>
+<script src="assets/js/leaflet-providers.js"></script>
+<script src="assets/js/leaflet.markercluster.js"></script>
+<script src="assets/js/slick.min.js"></script>
+<script src="assets/js/maps.js"></script>
+<script src="assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB0N5pbJN10Y1oYFRd0MJ_v2g8W2QT74JE"></script>
+<script src="assets/js/ie-emulation-modes-warning.js"></script>
+<!-- Custom JS Script -->
+<script  src="assets/js/app.js"></script>
+
+</body>
+</html>
